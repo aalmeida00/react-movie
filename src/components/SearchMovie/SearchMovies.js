b39d25bef8abcb992dfd3ac642dc96a65ecd7941
Andre Almeida <andre-silva78@hotmail.com>
2021-03-02 00:15:05 -0300

    build: basic fetch api

--- src/components/SearchMovie/SearchMovies.js
+++ src/components/SearchMovie/SearchMovies.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const SearchMovies = () => {
+  const procurarLivros = async (e) => {
+    e.preventDefault();
+    const query = 'Avengers';
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=9adfe29ab2c581d7d5fae07da955d98d&language=en-US${query}`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="search-bar">
-      <form action="" className="form">
+      <form onSubmit={procurarLivros} action="" className="form">
         <label className="form--label" htmlFor="query">
           Nome do livro
         </label>
