237fb52c3182849716b6abf60e80eb3c53fa9791
Andre Almeida <andre-silva78@hotmail.com>
2021-03-02 00:50:18 -0300

    build: movie the movie card to a object and consume as object with destructuring props

--- src/components/SearchMovie/SearchMovies.js
+++ src/components/SearchMovie/SearchMovies.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MovieCards from '../MovieCards/MovieCards';
 
 const SearchMovies = () => {
   const [query, setQuery] = useState('');
@@ -38,29 +39,7 @@ const SearchMovies = () => {
           </button>
         </form>
       </div>
-      <div className="card-list">
-        {movies
-          .filter((movie) => movie.poster_path)
-          .map((movie) => (
-            <div key={movie.id} className="card">
-              <img
-                className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + 'poster'}
-              />
-              <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p>
-                  <small>LANÇAMENTO: {movie.release_date}</small>
-                </p>
-                <p>
-                  <small>NOTA: {movie.vote_average}</small>
-                </p>
-                <p className="card--desc">{movie.overview}</p>
-              </div>
-            </div>
-          ))}
-      </div>
+      <MovieCards movies={movies} />
     </>
   );
 };
