fbbc3ffe19697d0dda2dc10c69f44ba006a13848
Andre Almeida <andre-silva78@hotmail.com>
2021-03-02 00:27:58 -0300

    feat: useState to manage the movies and fetch proprely the data

--- src/components/SearchMovie/SearchMovies.js
+++ src/components/SearchMovie/SearchMovies.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 
 const SearchMovies = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
   const procurarLivros = async (e) => {
     e.preventDefault();
-    const query = 'Jurassic Park';
+
     const api_key = '9adfe29ab2c581d7d5fae07da955d98d';
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}`;
     try {
       const res = await fetch(url);
       const data = await res.json();
 
-      console.log(data);
+      setMovies(data.results);
+
+      console.log(movies);
     } catch (err) {
       console.log(err);
     }
@@ -26,6 +31,8 @@ const SearchMovies = () => {
           name="query"
           placeholder="Procure por um livro"
           type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="form--button" type="submit">
           Procurar
