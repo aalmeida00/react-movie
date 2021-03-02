cfc114295d1632104096c49be96054dc0e084ea9
Andre Almeida <andre-silva78@hotmail.com>
2021-03-02 00:37:39 -0300

    feat: display movie information

--- src/components/SearchMovie/SearchMovies.js
+++ src/components/SearchMovie/SearchMovies.js
@@ -14,31 +14,54 @@ const SearchMovies = () => {
       const data = await res.json();
 
       setMovies(data.results);
-
-      console.log(movies);
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <div className="search-bar">
-      <form onSubmit={procurarLivros} action="" className="form">
-        <label className="form--label" htmlFor="query">
-          Nome do livro
-        </label>
-        <input
-          className="form--input"
-          name="query"
-          placeholder="Procure por um livro"
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button className="form--button" type="submit">
-          Procurar
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="search-bar">
+        <form onSubmit={procurarLivros} action="" className="form">
+          <label className="form--label" htmlFor="query">
+            Nome do livro
+          </label>
+          <input
+            className="form--input"
+            name="query"
+            placeholder="Procure por um livro"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className="form--button" type="submit">
+            Procurar
+          </button>
+        </form>
+      </div>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div key={movie.id} className="card">
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + 'poster'}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>LANÇAMENTO: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>NOTA: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
